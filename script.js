44320299a4e00c2b695e28ff690ce6bb69aba883
Clara Loforti <clara.loforti@gmail.com>
2025-12-03 09:36:04 +0100

    sistemo indentatura

--- script.js
+++ script.js
@@ -16,15 +16,11 @@ let contenutoLista = '';
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) { // se il numero è multiplo sia di 3 che di 5 stampo FizzBuzz
         contenutoLista += `<li class="text-primary">FizzBuzz</li>`;
-    }
-    else if (i % 3 === 0) { // se il numero è multiplo di 3 stampo Fizz 
+    } else if (i % 3 === 0) { // se il numero è multiplo di 3 stampo Fizz 
         contenutoLista += `<li class="text-warning">Fizz</li>`;
-    }
-
-    else if (i % 5 === 0) { // se il numero è multiplo di 5 stampo Buzz
+    } else if (i % 5 === 0) { // se il numero è multiplo di 5 stampo Buzz
         contenutoLista += `<li class="text-danger">Buzz</li>`;
-    }
-    else { // altrimenti stampo il numero
+    } else { // altrimenti stampo il numero
         contenutoLista += `<li class="text-success"> ${i} </li>`;
     }
 }
